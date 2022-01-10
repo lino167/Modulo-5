892e1ffd4e14652b404ec6700b2ed699eb9a7c32
lino167 <97192586+lino167@users.noreply.github.com>
2022-01-10 09:37:48 -0300

    Subindo arquivo

---
+++ wrappers/Criandowrappers.java
@@ -0,0 +1,29 @@
+package wrappers;
+
+public class Criandowrappers {
+
+    public static void main(String[] args) {
+        
+    short num1 = 1;
+    byte num2 = 2;
+    int num3 = 3;
+    long num4 = 4;
+    float num5 = 5.5f;
+    double num6 = 6.6666;
+    boolean flag = true;
+    char a = 'A';
+
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num3);
+    System.out.println(num4);
+    System.out.println(num5);
+    System.out.println(num6);
+    System.out.println(flag);
+    System.out.println(a);
+    
+
+        
+    }
+    
+}
